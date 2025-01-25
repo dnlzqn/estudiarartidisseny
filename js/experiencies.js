88c90e6d6f8d39d3802bd231f71ecc70ecfa6eec
Daniel Uzquiano <55982609+dnlzqn@users.noreply.github.com>
2025-01-25 13:51:01 +0100

    Add files via upload

--- js/experiencies.js
+++ js/experiencies.js
@@ -1,148 +1,48 @@
-window.slider = {};
+const sliderContainer = document.querySelector('.slider-container');  // Contenedor visible
+const slider = document.querySelector('.slider');
+const slides = document.querySelectorAll('.slide');
+let currentIndex = 5;  // Iniciar desde el slide correspondiente a 500vw
+document.getElementById('prevBtn').style.opacity = '0'; 
 
-slider.container = document.querySelector('.slider');
-slider.containerWidth = slider.container.offsetWidth;
-slider.slides = document.querySelectorAll('.slider__item');
-slider.slidesWrapper = document.querySelector('.slides');
-slider.sliderCounter = slider.slides.length;
-slider.currentSlide = slider.slides[0];
-slider.dots = document.querySelectorAll('.dots__dot');
-slider.currentDot = slider.dots[0];
-slider.num = 0;
-slider.paneWidth = (slider.sliderCounter * slider.containerWidth) / slider.sliderCounter;
-slider.slidesWrapper.style.width = slider.sliderCounter * slider.containerWidth + 'px';
-
-slider.moveSlide = function(ev) {
-    slider.currentSlide.classList.remove('slider__item--current');
-    slider.currentDot.classList.remove('dots__dot--current');
-
-    slider.currentSlide = slider.slides[slider.num];
-    slider.currentDot = slider.dots[slider.num];
-
-    slider.currentSlide.classList.add('slider__item--current');
-    slider.currentDot.classList.add('dots__dot--current');
-
-    slider.slidesWrapper.classList.add('is-animating');
-    clearTimeout(slider.timer);
-    slider.timer = setTimeout(function() {
-        slider.slidesWrapper.classList.remove('is-animating');
-    }, 400);
-
-    slider.animate = (slider.containerWidth * slider.num * -1) + 'px';
-    slider.slidesWrapper.style.left = slider.animate;
-    slider.slidesWrapper.style.transform = '';
-}
-
-slider.nextItem = function() {
-    slider.num++;
-    slider.num = slider.num % slider.sliderCounter;
-
-    slider.moveSlide(slider.slides[slider.num]);
-}
-
-slider.prevItem = function() {
-    if (slider.num === 0) {
-        slider.num = slider.sliderCounter;
-    }
-
-    slider.num--;
-
-    slider.moveSlide(slider.slides[slider.num]);
-}
-
-slider.dotsClick = function(el) {
-    el = el.target;
-
-    for (var i = 0; i < slider.dots.length; i++) {
-        if (slider.dots[i] === el) {
-            slider.num = i;
-        }
-    }
-
-    slider.moveSlide(slider.num);
-}
-
-slider.panSlide = function() {
-
-    var hammer = new Hammer.Manager(slider.slidesWrapper);
-
-
-
-    hammer.add(new Hammer.Pan({
-        threshold: 0,
-        pointers: 0
-    }));
-
-
-    hammer.on('pan', function(e) {
-        slider.currentPane = Math.max(0, Math.min(slider.num, slider.sliderCounter - 1));
-
-        var ContainerOffsetX = -slider.currentPane * slider.paneWidth + e.deltaX;
-
-        slider.slidesWrapper.style.left = ContainerOffsetX + 'px';
-    });
-
-    hammer.on('panend', function(e) {
-        if (e.deltaX > 1) {
-            slider.goTo(slider.num - 1);
-        } else if (e.deltaX < -1) {
-            slider.goTo(slider.num + 1)
-        } else {
-            slider.goTo(slider.num);
-        }
-
-        slider.currentSlide = slider.slides[slider.num];
-        slider.currentDot = slider.dots[slider.num];
-    });
-}
-
-slider.goTo = function(number) {
-    if (number < 0) {
-        slider.num = 0;
-    } else if (number > slider.sliderCounter - 1) {
-        slider.num = slider.sliderCounter - 1;
+function updateSlider() {
+    if (currentIndex <= -5) {
+        currentIndex = -5;
+        document.getElementById('nextBtn').style.opacity = '0';
+    } else if (currentIndex >= 5) {
+        currentIndex = 5;
+        document.getElementById('prevBtn').style.opacity = '0';  
     } else {
-        slider.num = number;
+      document.getElementById('nextBtn').style.opacity = '1'; 
+      document.getElementById('prevBtn').style.opacity = '1'; 
     }
 
-    slider.currentPane = Math.max(0, Math.min(slider.num, slider.sliderCounter - 1));
-
-    var ContainerOffsetX = -slider.currentPane * slider.paneWidth;
-
-    slider.slidesWrapper.style.left = ContainerOffsetX + 'px';
-
-    slider.slidesWrapper.classList.add('is-animating');
-    clearTimeout(slider.timer);
-    slider.timer = setTimeout(function() {
-        slider.slidesWrapper.classList.remove('is-animating');
-    }, 400);
-
-    for (var i = 0; i < slider.sliderCounter; i++) {
-        i === slider.num ?
-            slider.slides[i].classList.add('slider__item--current') :
-            slider.slides[i].classList.remove('slider__item--current');
-    }
-
-    for (var n = 0; n < slider.dots.length; n++) {
-        n === slider.num ?
-            slider.dots[n].classList.add('dots__dot--current') :
-            slider.dots[n].classList.remove('dots__dot--current');
-    }
-};
-
-
-for (var i = 0; i < slider.dots.length; i++) {
-    slider.dots[i].addEventListener('click', slider.dotsClick);
+    slider.style.transform = `translateX(${currentIndex * 100}vw)`;
 }
 
-slider.panSlide();
-
-
-
-function avanzarAutomaticamente() {
-    setInterval(function() {
-        slider.nextItem();
-    }, 30000);
+function nextSlide() {
+    currentIndex--;
+    updateSlider();
 }
 
-avanzarAutomaticamente();
+function prevSlide() {
+    currentIndex++;
+    updateSlider();
+}
+
+// Event listeners para los botones de navegación
+document.getElementById('nextBtn').addEventListener('click', nextSlide);
+document.getElementById('prevBtn').addEventListener('click', prevSlide);
+
+// Integración de Hammer.js para detectar gestos táctiles
+const hammer = new Hammer(document.querySelector('.experiencies'));
+
+
+hammer.on('swipeleft', () => {
+    console.log('Swiped left');
+    nextSlide();
+});
+
+hammer.on('swiperight', () => {
+    console.log('Swiped right');
+    prevSlide();
+});
